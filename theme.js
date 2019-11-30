93f3391456da11e856eeb8e2720f3ca4a1cc8f7d
Atanas Angelov <hi@atanas.dev>
2019-11-30 21:04:35 +0200

    Add basic slide outline

--- theme.js
+++ theme.js
@@ -1,7 +1,19 @@
+import background from './static/background.png';
+
 export const theme = {
   // Customize your presentation theme here.
   //
   // Read the docs for more info:
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/theming.md
   // https://github.com/jxnblk/mdx-deck/blob/master/docs/themes.md
+  fonts: {
+    heading: 'Montserrat',
+  },
+  styles: {
+    Slide: {
+      borderTop: '3px solid #5e75ff',
+      borderBottom: '3px solid #5e75ff',
+      background: '#FFFFFF url(' + background + ') no-repeat center bottom',
+    },
+  },
 }
